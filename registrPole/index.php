8b41b0f0253c0d90503540fba40a068bed307355
Kodelarcher <151816165+Kodelarcher@users.noreply.github.com>
2024-03-21 15:57:34 +0300

    Update and rename index.html to index.php

--- registrPole/index.php
+++ registrPole/index.php
@@ -32,5 +32,37 @@
         </div>
       </div>
     </div>
+    <?php
+require_once('connect.php');
+if (!empty($_REQUEST['login']) && !empty($_REQUEST['phone']) && !empty($_REQUEST['email']) && !empty($_REQUEST['password']));
+$login=$_POST['login'];
+$phone=$_POST['phone'];
+$email=$_POST['email'];
+$password=$_POST['password'];
+$repassword=$_POST['repassword'];
+if ($password !=$repassword) {
+    echo 'Пароль не совпадает';
+}
+else{
+    $passw=password_hash($password, PASSWORD_BCRYPT);
+    $sql="INSERT INTO users1 (login, phone, email, password) values ('$login', '$phone', '$email', '$passw')";
+    if ($db ->query($sql)=== TRUE) {
+        echo "Успешная регистрация";
+}
+else {
+    echo "Ошибка:" .$db->error;
+}
+}
+?>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-23581568-13');
+	</script>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v52afc6f149f6479b8c77fa569edb01181681764108816" integrity="sha512-jGCTpDpBAYDGNYR5ztKt4BQPGef1P0giN6ZGVUi835kFF88FOmmn8jBQWNgrNd8g/Yu421NdgWhwQoaOPFflDw==" data-cf-beacon='{"rayId":"7c858714db7216d3","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2023.4.0","si":100}' crossorigin="anonymous"></script>
   </body>
 </html>
